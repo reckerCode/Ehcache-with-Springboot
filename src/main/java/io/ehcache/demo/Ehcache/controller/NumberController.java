b4c6edac485d4b91795fd047f37dfa3a2e64f04f
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-07-29 14:38:05 -0400

    made changes to the annotations used for get mapping

--- src/main/java/io/ehcache/demo/Ehcache/controller/NumberController.java
+++ src/main/java/io/ehcache/demo/Ehcache/controller/NumberController.java
@@ -5,20 +5,24 @@ import io.ehcache.demo.Ehcache.service.NumberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/number",consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/number")
 public class NumberController {
 
     @Autowired
     public NumberService numberService;
 
-    public String getSquare(@PathVariable Long number){
-        log.info("Calling NumberService to get the square of number {}", number);
-        return String.format("{\"square\": %s}", numberService.square(number));
+
+    @GetMapping("/square/{number}")
+    public String getSquare(@PathVariable("number") String number){
+        long temp = Long.parseLong(number);
+        log.info("Calling NumberService to get the square of number {}", temp);
+        return String.format("{\"square\": %s}", numberService.square(temp));
     }
 }
