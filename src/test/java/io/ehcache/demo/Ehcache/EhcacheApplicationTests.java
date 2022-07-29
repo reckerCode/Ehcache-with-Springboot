202c137f557331c00cd31123c5da689d014b59a9
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-07-29 13:38:28 -0400

    First commit

---
+++ src/test/java/io/ehcache/demo/Ehcache/EhcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ehcache.demo.Ehcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EhcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
