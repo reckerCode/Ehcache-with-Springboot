8e009c6c93caf93fec70359fc151dbfdc25fbab4
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-07-29 14:41:27 -0400

    describing the use of cacheable annotation

--- src/main/java/io/ehcache/demo/Ehcache/service/NumberService.java
+++ src/main/java/io/ehcache/demo/Ehcache/service/NumberService.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
+/**
+ * Here we annotated method with Cacheable do that Spring
+ * will handle the caching. As a result of this annotation,
+ * Spring will create a proxy of the NumberService to intercept
+ * calls to the square method and call Ehcache.
+ */
 @Service
 @Slf4j
 public class NumberService {
