bb5cf65afa2b71f1034cdcef9f3327bf2b056bdc
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-07-29 14:03:33 -0400

    cacheable service layer method

--- src/main/java/io/ehcache/demo/Ehcache/service/NumberService.java
+++ src/main/java/io/ehcache/demo/Ehcache/service/NumberService.java
@@ -1,2 +1,21 @@
-package io.ehcache.demo.Ehcache.service;public class NumberService {
+package io.ehcache.demo.Ehcache.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@Slf4j
+public class NumberService {
+
+    @Cacheable(value = "squareCache",
+            key = "#number",
+            condition = "#number>10")
+    public BigDecimal square(Long number){
+        BigDecimal bigDecimalSquare = BigDecimal.valueOf(number).multiply(BigDecimal.valueOf(number));
+        log.info("Square of {} is {}",number,bigDecimalSquare);
+        return bigDecimalSquare;
+    }
 }
