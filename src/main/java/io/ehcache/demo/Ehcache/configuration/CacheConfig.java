7d7db9a64dfdb62cb07478e283712517fe1363cc
Hardik Gunjal <hardikgunjal@Hardiks-MacBook-Pro.local>
2022-07-29 14:37:01 -0400

    Cache configuration file

---
+++ src/main/java/io/ehcache/demo/Ehcache/configuration/CacheConfig.java
@@ -0,0 +1,10 @@
+package io.ehcache.demo.Ehcache.configuration;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+}
